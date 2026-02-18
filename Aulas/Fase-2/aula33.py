4533e732cd5d9f887c0ac76b8937b6c77092942d
Demetrios10 <demetrios.10@hotmail.com>
2026-02-17 22:31:12 -0300

    Up

--- Aulas/Fase-2/aula33.py
+++ Aulas/Fase-2/aula33.py
@@ -1,5 +1,7 @@
 frase = 'O python é uma linguagem de\
-    programação multiparadgma.\
-    Python foi criado por Guido Van Rossum'
+ programação multiparadgma.\
+ Python foi criado por Guido Van Rossum'
     
+print(frase.count('Python')) # quantas vezes apareceu a palavra Python dentro da frase
+
 
---
+++ Projetos-Pessoais/Projeto-27.py
@@ -0,0 +1,18 @@
+# Verificação de numero na lista de convidados 
+
+while True:
+  numero = input('Digite um numero: ')
+  nome = input('Digite seu nome: ')
+ 
+  nomes = ['demetrios','jose','amanda','antonio','marcia','quiteria','fabio','domingos','david','josiel']
+  lista = ['22','0','35','45','78','99','100','23','43','75']
+ 
+  if numero and nome in nomes and lista:
+   print('Numero e nome estão na lista de convidados :)')
+  else:
+   print('Numero e nome não estão na lista de convidados :(')
+    
+  sair = input('Quer sair do sistema? [s]im: ').lower().startswith('s')
+  if sair is True:
+   print('vc saiu!!') 
+   break
