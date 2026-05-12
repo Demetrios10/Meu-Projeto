b0e5ea2c96d7c56697b6098987589e295e7327e0
Demetrios Alves <demetrios.10@hotmail.com>
2026-05-12 11:37:35 -0300

    update

--- curso_python/aula1.py
+++ curso_python/aula1.py
@@ -1,9 +1,23 @@
-# comentarios em python
+# Comentarios em Python 
 
 
-print('Hello World !') # comentario a frente 
-print('Demetrios')
-print('isso é python')
-# comentario abaixo 
+print("voce pode usar comentarios em python em qualuqer lugar no seu codigo")
 
-    
+# podendo usar na frente , abaixo do código e acima do mesmo
+
+
+print("se vc quiser pode usar tb o comentario de multip´las linhas")
+
+'''
+assim comentamos em varias linhas ao mesmo tempo usando 3 aspas simples ou duplas
+
+'''
+
+print("A função print imprime uma mensagem em tela")
+# o comando print é usado para imprimir uma mensagem em tela
+
+
+print("Tb temos Docstrings para comentar nosso código")
+""" 
+vc pode usar para escrever notas ou para documentar o código usando 3 aspas duplas ou simples
+"""
